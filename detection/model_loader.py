ecd3dd481e1f7bac94fc2fe0b73a0d369c274fbb
Ciman Code <firmanabdul278@gmail.com>
2025-07-20 21:33:51 +0700

    (+) fixing

--- detection/model_loader.py
+++ detection/model_loader.py
@@ -8,12 +8,22 @@ def download_fasterrcnn_model():
     model_url = "https://huggingface.co/firmanabdlrhmn/fasterrcnn_model/resolve/main/fasterrcnn_best_model.pth"
     model_dir = "models"
     model_path = os.path.join(model_dir, "fasterrcnn_best_model.pth")
+
     if not os.path.exists(model_path):
         os.makedirs(model_dir, exist_ok=True)
-        print("Downloading Faster R-CNN...")
-        r = requests.get(model_url)
-        with open(model_path, 'wb') as f:
-            f.write(r.content)
+        print("Downloading Faster R-CNN model...")
+
+        try:
+            with requests.get(model_url, stream=True, timeout=30) as r:
+                r.raise_for_status()
+                with open(model_path, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        if chunk:
+                            f.write(chunk)
+            print("Download selesai.")
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError(f"Gagal mengunduh model dari Hugging Face: {e}")
+
     return model_path
 
 @lru_cache(maxsize=1)
